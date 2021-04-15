04e645610c5ea51d4799f5183a0c9adce26aca22
Jessicahaugen <j.rose.haugen@gmail.com>
2021-04-15 12:11:08 -0700

    final

--- parksAPI.js
+++ parksAPI.js
@@ -20,123 +20,109 @@ const getParks = async (activityName, onlyFavorites) => {
       }
       return true;
     });
-  console.log(convertInfo.data);
+
 
   parkList.innerHTML = "";
 
   for (let i = 0; i < convertInfo.data.length; i++) {
     let parkContainer = document.createElement("div");
-    parkContainer.className = "parkContainer";
+    parkContainer.className = `container-sm`;
     parkContainer.id ="parkCard"
+    let imgDiv = document.createElement("div");
+    imgDiv.className="imageDiv";
     let mainImg = document.createElement("img");
     mainImg.className = "parkImage";
-    mainImg.height = "300";
-    mainImg.width = "300";
+    mainImg.height = "325";
+    mainImg.width = "325";
     mainImg.src = convertInfo.data[i].images[0].url;
+    let infoDiv = document.createElement("div");
+    infoDiv.className="infoDiv";
     let parkState = document.createElement("h3");
     parkState.innerHTML = `State: ${convertInfo.data[i].states}`;
     let parkInfo = document.createElement("p");
     parkInfo.innerText = convertInfo.data[i].description;
     let parkName = document.createElement("h2");
+    parkName.className= "parkNames";
     parkName.innerHTML = convertInfo.data[i].fullName;
     let heart = document.createElement("button");
-    heart.innerHTML = '<img src="heart.jpeg" width="20px" height="20px">';
+    heart.innerHTML = `add to favorites <i class="far fa-heart"></i>`;
     heart.className = "heart";
 
-    // let heartBtn = document.querySelector(".heart");
     heart.addEventListener("click", function () {
       favorites.push(convertInfo.data[i].id);
-    //   heart.className += " favorited";
-    });
-    parkContainer.append(mainImg, parkName, parkState, parkInfo, heart);
+      heart.className += " favorited";
+      alert("This park has been added to your favorites! Good luck on your Adventures!");
+    })
+    infoDiv.append(parkName, parkState, parkInfo, heart)
+    imgDiv.append(mainImg);
+    parkContainer.append(imgDiv, infoDiv);
     parkList.append(parkContainer);
   }
 };
 
-// create a list of favorites
-// const createFavorites = () => {
-
-    // }
-let deleteContainer = (id) => { 
-document.getElementsByClassName(`${id}`).remove();
-}
-
 //button event listeners to change activity name
+let filterHeader = document.querySelector(".filterTitle");
+
 let hikingBtn = document.querySelector("#hiking");
 hikingBtn.addEventListener("click", function () {
   getParks("Hiking");
-  parkList.innerHTML="";
+  filterHeader.innerHTML = "Take a Hike at:";
 });
 let skiingBtn = document.querySelector("#skiing");
 skiingBtn.addEventListener("click", function () {
   getParks("Skiing");
-  parkList.innerHTML=""
+  filterHeader.innerHTML = "Hit the Slopes at:";
+  
 });
 let bikingBtn = document.querySelector("#biking");
 bikingBtn.addEventListener("click", function () {
   getParks("Biking");
-  parkList.innerHTML="";
+  filterHeader.innerHTML = "Cycle in Nature at:";
+
 });
 let campingBtn = document.querySelector("#camping");
 campingBtn.addEventListener("click", function () {
   getParks("Camping");
-  deleteContainer(".parkCard")
+  filterHeader.innerHTML = "Cycle in Nature at:";
+ 
 });
 let birdBtn = document.querySelector("#birdwatching");
 birdBtn.addEventListener("click", function () {
   getParks("Birdwatching");
+  filterHeader.innerHTML = "Discover the birds at:";
+  
 });
 let fishingBtn = document.querySelector("#fishing");
 fishingBtn.addEventListener("click", function () {
   getParks("Fishing");
+  filterHeader.innerHTML = "Fish in Peace at:";
 });
 let toursBtn = document.querySelector("#tours");
 toursBtn.addEventListener("click", function () {
   getParks("Guided Tours");
+  filterHeader.innerHTML = "Learn the terrain with tours at:";
 });
 let horseBtn = document.querySelector("#horseback");
 horseBtn.addEventListener("click", function () {
   getParks("Horseback Riding");
+  filterHeader.innerHTML = "Hit the trails on horseback!";
 });
-//Function to filter API by Activity name and return appropriate parks REFACTORED INTO ONE FUNCTION!
-// const getActivity = async (activityName) => {
-//     const getInfo = await fetch ("https://developer.nps.gov/api/v1/parks?stateCode=WA,OR,ID&api_key=HUxRiqwN849LGQztfrgmnxS3egwTOmsLJp8fXvEH");
-//     const convertInfo = await getInfo.json();
-//     console.log(convertInfo);
-
-//     const matchingParks = convertInfo.data.filter(d => d.activities.find(a => a.name === activityName))
-//     console.log(matchingParks)
-
-//     for (parks of matchingParks){
-//         let i = 0;
-//         let parkContainer = document.createElement("div");
-//         parkContainer.class = "parkContainer";
-//         let mainImg = document.createElement("img");
-//         mainImg.className = "parkImage";
-//         mainImg.height = "300";
-//         mainImg.width = "300";
-//         mainImg.src =  parks.images[0].url;
-//         let parkState = document.createElement("h3")
-//         parkState.innerHTML=`State: ${parks.states}`;
-//         let parkInfo= document.createElement("p");
-//         parkInfo.innerText = parks.description;
-//         let parkName = document.createElement("h2");
-//         parkName.innerHTML = parks.fullName;
-//         parkContainer.append(mainImg,parkName,parkState,parkInfo,);
-//         parkList.append(parkContainer);
-//         i += 1;
-//     }
-// }
-// getActivity();
+  
 let removeButtons = document.querySelector(".buttons");
-let seeAllBtn = document.querySelector(".seeAll");
+let seeAllBtn = document.querySelector("#mainBtn");
 seeAllBtn.addEventListener("click", function () {
   getParks();
   removeButtons.innerHTML = "";
+  filterHeader.innerHTML = "Collection of all National parks in the Pacific NW";
+  
 });
 
-    
-    // show favorites
-    // getParks(null, true)
+let favoritedBtn = document.querySelector(".favorites");
+favoritedBtn.addEventListener("click", function () {
+  getParks(null,true);
+  filterHeader.innerHTML = `Favorites <i class="fas fa-heart"></i>`;
+  
+});
+
 
 
